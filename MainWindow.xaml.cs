9e1a8a9a2becae03eb1905f5bca04d8afcea712c
Ethan Hunter <24462157+Racnera@users.noreply.github.com>
2019-05-03 11:31:31 -0400

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,17 +19,49 @@ namespace Sprint_2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+        enum GameState { MainMenu,Loading,TrackSelection,Time,GameOn}
+    
     public partial class MainWindow : Window
     {
+        //global
+        public DispatcherTimer gametimer = new DispatcherTimer();
+        Tracks t = new Tracks();
+        public static int trackNum;
+        GameState gameState = new GameState();
+        Player p1 = new Player();
+
         public MainWindow()
-        {
+        {   
             InitializeComponent();
+            gameState = GameState.MainMenu;
+            gametimer.Tick += Gametimer_Tick;
+            gametimer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);
+            gametimer.Start();
+        }
+
+        private void Gametimer_Tick(object sender, EventArgs e)
+        {
+            p1.update(canvas);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             TrackSelection TS = new TrackSelection();
             TS.ShowDialog();
+        }
+
+        private void startGame_Click(object sender, RoutedEventArgs e)
+        {
+            if (trackNum == 2)
+            {
+                gameState = GameState.Loading;
+                selectTrack.Visibility = Visibility.Hidden;
+                startGame.Visibility = Visibility.Hidden;
+                p1.drawPlayer(canvas);
+                t.Track2(canvas);
+            }
         }
     }
 }
